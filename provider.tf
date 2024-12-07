4bff347e314bd4eb8e35bde06f337215b1379810
Ehsan Eskandari <Ehsan_Eskandari@hotmail.com>
2024-12-07 10:54:43 -0500

    update snowflake credentils

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "snowflake" {
-  user = var.SNOWFLAKE_USERNAME
-  password = var.SNOWFLAKE_PASSWORD
-  account_name      = var.SNOWFLAKE_ACCOUNT_NAME
-  organization_name = var.SNOWFLAKE_ORGANIZATION_NAME
   role     = "SYSADMIN"
 }
